0d1aa6e9d9725c56258cfaa48df83ce39a6856cf
kimsb <kim.stephen.bovim@bekk.no>
2019-12-17 12:25:18 +0100

    heroku setup

--- src/main/kotlin/no/nsf/scrabbellerserver/App.kt
+++ src/main/kotlin/no/nsf/scrabbellerserver/App.kt
@@ -1,22 +1,37 @@
 package no.nsf.scrabbellerserver
 
 import io.ktor.application.call
-import io.ktor.http.HttpStatusCode
+import io.ktor.application.install
+import io.ktor.features.CORS
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
+import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
-fun main(args: Array<String>) {
-    embeddedServer(Netty,23567) {
+fun main() {
+    val port = System.getenv("PORT")?.toInt() ?: 23567
+    embeddedServer(Netty,port) {
+        install(CORS) {
+            anyHost()
+        }
+        install(ContentNegotiation) {
+            gson {
+                setPrettyPrinting()
+            }
+        }
         routing {
             get("") {
                 call.respond("I'm alive!")
             }
             get("hello") {
-                call.respond(HttpStatusCode.Accepted, "Hello")
+                call.respond(OK, Spiller("Kim", 2000.1))
             }
         }
     }.start(wait = true)
 }
+
+data class Spiller(val navn: String, val rating: Double)
